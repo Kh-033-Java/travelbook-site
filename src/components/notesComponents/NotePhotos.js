54f87aee63864cd83dd0703308ac4553abd1adce
Nastya <nastyasischuk@gmail.com>
2019-11-04 14:19:00 +0300

    show note finished basic components

--- src/components/notesComponents/NotePhotos.js
+++ src/components/notesComponents/NotePhotos.js
@@ -3,19 +3,20 @@ import './NoteStyling.css'
 import '../App.css';
 import { Carousel } from 'react-responsive-carousel';
 import   'react-responsive-carousel/lib/styles/carousel.min.css';
-import logo from '../icons/icon3.jpg'
 function NotePhotos(props){
 
     return (
   <div className ="note-photos container">
-   <Carousel  showArrows={true} >
-   <div>
-                    <img src={logo} alt=""/>
-                </div>
-                <div>
-                    <img src={logo}  alt="" />
-                </div>
+      <div className="title-note">Photos</div>
+      <div  className="carouseel">
+   <Carousel  showArrows={true} showThumbs={false} dynamicHeight={true} dynamicWidth={true}>
+       {props.photos.map(photo=> 
+             <div className="car-img">
+           <img src={photo} alt=""/>
+       </div>
+       )}
    </Carousel>
+   </div>
           </div>
 
     );
