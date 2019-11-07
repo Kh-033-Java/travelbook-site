02324b0d5b9a9c8ded480d93d006677c60fab7e9
DestroStar <dstarostin09@gmail.com>
2019-11-07 21:11:18 +0200

    small GI fixes

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -41,8 +41,10 @@ class GeneralInfo extends Component {
     }
 
     render() {
+        if(this.state.description !== undefined && this.state.weather !== undefined && this.state.generalInfo !== undefined){ //todo refactor
         return (
             <aside className="rightbar container" style={{overflow: 'auto'}}>
+                
                 <h1>General Information about country</h1>
                 <p>Country name - {this.state.generalInfo.name}</p>
                 <p>Capital - {this.state.description.capital} </p>
@@ -52,9 +54,17 @@ class GeneralInfo extends Component {
                 <p>Description - {this.state.description.commonInformation}</p>
                 <p>Cuisine - {this.state.description.cuisine}</p>
                 <p>Climate - {this.state.description.climate}</p>
+                
             </aside>
         )
+    } else
+    {
+        return (
+            <aside className="rightbar container" style={{overflow: 'auto'}}>
+                </aside>
+        );
     }
+}
 }
 
 export default GeneralInfo;
