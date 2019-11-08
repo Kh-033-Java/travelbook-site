7cbbed86deb39449504105b507995bce4eda1f5e
DestroStar <dstarostin09@gmail.com>
2019-11-09 01:17:32 +0200

    GI loading fix

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 import {Route, NavLink, Redirect} from 'react-router-dom';
 import Loading from "./Loading";
 
+
+
 class GeneralInfo extends Component {
 
     constructor(props) {
@@ -13,20 +15,31 @@ class GeneralInfo extends Component {
             description: [{}],
             weather: [{}],
             photos: [{}],
-            isInfoValid: true,
+            isInfoValid: false,
             isLoading: true
         };
+    }
+
+    checkInfoValid(generalInfo) {
+
+        if(generalInfo.description === undefined || generalInfo.name === "undefined"){
+            this.setState({isInfoValid: false});
+            return false;
+        }else{
+            this.setState({isInfoValid: true});
+            return true;
+        }
     }
 
     componentDidMount() {
 
         const endpoint = `http://localhost:8080/country/` + this.props.name + `/description`;
         console.log(endpoint);
-        const isLoading = false;
-
+        const isLoading =false;
         axios.get(endpoint)
             .then(response => {
                 const generalInfo = response.data;
+                if(this.checkInfoValid(generalInfo)){
                 const description = response.data.description;
                 const weather = response.data.weather;
                 const photos = response.data.photos;
@@ -34,14 +47,24 @@ class GeneralInfo extends Component {
                 this.setState({description});
                 this.setState({weather});
                 this.setState({photos});
+                }
                 this.setState({isLoading});
+
             }).catch(error => {
             console.log("Could not get GI from backend");
             this.setState({
-                isInfoValid: false
+                    isInfoValid: false
             });
+            if(this.description === undefined){
+            this.setState({isInfoValid: false});
+
+            }
+            
         });
     };
+
+    
+
 
     render() {
         const spinner = this.state.isLoading ? <Loading/> : null;
@@ -49,7 +72,6 @@ class GeneralInfo extends Component {
                 return (
                     <aside className="rightbar container" style={{overflow: 'auto'}}>
                         <h1>General Information about country</h1>
-                        {spinner}
                         <p>Country name - {this.state.generalInfo.name}</p>
                         <p>Capital - {this.state.description.capital} </p>
                         <p className="weather">Current weather in capital {this.state.weather.temperature}, {" "}
@@ -64,11 +86,18 @@ class GeneralInfo extends Component {
                     </aside>
                 );
             }
-            else {
+            else if(this.state.isLoading) {
                 return (
                     <aside className="rightbar container" style={{overflow: 'auto'}}>
                         <h1>General Information about country</h1>
-                        <p>No info</p>
+                        <Loading/> 
+                    </aside>
+                );
+            } else{
+                return (
+                    <aside className="rightbar container" style={{overflow: 'auto'}}>
+                        <h1>General Information about country</h1>
+                        <p>no info</p>
                     </aside>
                 );
             }
