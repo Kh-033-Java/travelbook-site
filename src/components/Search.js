ba8ae0032cf599d0eb95b5ab4b603d903660efd0
DestroStar <dstarostin09@gmail.com>
2019-11-11 03:36:23 +0200

    add user search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,106 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css';
+import './searchComponents/Dropdown.css';
+import Axios from 'axios';
+import Checkbox from './searchComponents/Checkbox';
 
-function Search(){
+class Search extends Component {
 
-    return(
-        <div className="search">
-         <form name ="myForm">
-            <input type="text" placeholder="Search"/>
-                    </form>
-    </div>
-    )
+    constructor(props) {
+        super();
+        this.state = {
+            inputValue: '',
+            isUserSearch: true,
+            users: [],
+            displayData: []
+        };
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    async getAllUsers() {
+        const endpoint = 'http://localhost:8080/users/allUsers'
+        const response = await Axios.get(endpoint).then(async response => this.setState({ users: await response.data }));
+
+        console.log(this.state.users[0]);
+    }
+
+
+
+    addUserToDisplay(userData) {
+        return <a href={"http://localhost:8080/users/" + userData.login} style={{ display: '' }}>{userData.login}</a>
+    }
+
+
+    displayAllUsers() {
+        const users = this.state.users;
+
+        const displayData = [];
+        for (let i = 0; i < users.length; i++) {
+            displayData.push(this.addUserToDisplay(users[i]));
+
+        }
+        this.setState({ displayData });
+    }
+
+    filterFunction() {
+        const users = this.state.users;
+        const filtered = users.filter((user) => {
+            if (user.login.toLowerCase().startsWith(this.state.inputValue)) {
+                return user;
+            }
+        });
+
+        this.setState({ users: filtered });
+
+    }
+
+    onClick = () => {
+        this.setState({
+            inputValue: '',
+            displayData: ''
+        });
+        if (this.state.isUserSearch == false) {
+            this.setState({ isUserSearch: true });
+        } else {
+            this.setState({ isUserSearch: false });
+        }
+        console.log('state', this.state.isUserSearch);
+    }
+
+    async onChange(event) {
+        this.setState({ inputValue: event.target.value });
+        
+        if (this.state.isUserSearch) {
+            const allUsers = await this.getAllUsers();
+            this.filterFunction();
+            this.displayAllUsers();
+        }else{
+
+        }
+        
+        if (this.state.inputValue.length > 0) {
+            document.querySelector('.dropdown-content').style.display = 'block';
+        }
+        else {
+            document.querySelector('.dropdown-content').style.display = 'none';
+        }
+    }
+
+    render() {
+        return (
+            <div className="search">
+                <form name="myForm" className="search-form">
+                    <input type="text" placeholder={this.state.isUserSearch ? 'Search User' : 'Search Country'} value={this.state.inputValue} onChange={this.onChange} />
+                </form>
+                <div className='dropdown-content'>
+                    {this.state.displayData}
+                </div>
+                <Checkbox onClick={this.onClick} />
+
+            </div>
+        )
+    }
 }
+
 export default Search;
---
+++ src/components/searchComponents/Checkbox.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import './Checkbox.css';
+
+
+
+class CheckBox extends Component {
+    
+    constructor(props){
+        super();
+        this.onClick = this.onClick.bind(this);
+    }
+    onClick(e){
+        console.log('props2', this.props)
+
+        if(e.type === 'click')
+        {
+            this.props.onClick();
+        }
+    }
+    render(){
+    return (
+        <div className='check-box'>
+            <p>User</p>
+            <label  className="switch">
+                <input onClick={this.onClick} type="checkbox"/>
+                <span className="slider round"></span>
+            </label>
+            <p>Country</p>
+        </div>
+    );
+    }
+}
+export default CheckBox;
