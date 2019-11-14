0d6153fa11a4c8dce4ba09068288db27cd7cfd6b
dyominov <dema.ivan@gmail.com>
2019-11-15 00:41:45 +0200

    fix button search

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -7,7 +7,6 @@ import {avatar} from './user-page/avatar';
 import UnauthorizedSettings from './UnauthorizedSettings.js'
 import isAuthorized from './checker/authorizationChecker.js'
 import {MAIN_USER_LOGIN_ROUTE} from "../constants/constants";
-import SearchPlansHeader from "./search/SearchPlansHeader";
 
 function Settings(props){
     return(
@@ -17,7 +16,7 @@ function Settings(props){
             </React.Fragment>
             :<React.Fragment>
                 <LogOut/>
-                <div>
+                <div style={{display: "flex"}}>
                     <NavLink to="/search-plans">
                         <div className="button-plan" />
                     </NavLink>
